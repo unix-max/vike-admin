7921416e243db0d91e3cbc71764dbbb53d8a7b90
Max Chernyshev <unix_max@mail.ru>
2024-11-22 09:04:24 +0000

    Сделан Элемент списка Sklad

--- components/spr/SkladSpr/index.tsx
+++ components/spr/SkladSpr/index.tsx
@@ -2,92 +2,100 @@ import React from 'react'
 import { trpc } from "@/trpc/client";
 import { WindowCl } from '../../Window/winCl'
 import { SprButtons } from '../../SprButtons'
-import { ItemTable } from '../../ItemsTable'
+import { ItemTable } from '../../ItemsTable1'
 import { SkladElm } from './elm';
 import { useWinStore } from '@/pages/+client'
 import { ISklad} from '@/db/Entitys/Sklad'
 //import shallow from 'zustand/shallow'
-import styles from './styles'
+import styles from './styles.module.css'
+
 //console.log(styles)
 const addNWin = useWinStore.getState().addNWin;
+const delNWin = useWinStore.getState().delNWin
 
-export type IKlientSprProps = {
+export type ISkladSprProps = {
   winId: number;
   id?: number;
+  onChoice?: (elm:ISkladSprElm) => void;
 }
 
-type IKlientSprState = {
-  list: ISklad[];
+type ISkladSprElm = {
+  id: number,
+    name: string,
+	fullName?: string,
+	oksm: string, 
+	
 }
 
-export class SkladSpr extends React.Component<IKlientSprProps, IKlientSprState>{
+type ISkladSprState = {
+  list: ISkladSprElm[];
+}
+
+export class SkladSpr extends React.Component<ISkladSprProps, ISkladSprState>{
   selectElmId?: number;
-  itemRef: React.RefObject<ItemTable>;
+  itemRef: React.RefObject<ItemTable<ISkladSprElm>>;
 
-  constructor(props: IKlientSprProps) {
+  constructor(props: ISkladSprProps) {
     super(props);
     this.itemRef = React.createRef();
     this.state = {
-      list: [{id: 0, }]
+      list: []
   }
-  console.log(`Constructor KlientSpr ${this.props.id}`)
-  this.name = this.name.bind(this);
+  console.log(`Constructor SkladSpr Id:${this.props.id}`)
+
   }
   async componentDidMount() {
-    //console.log(initRequest)
-
-    const  data = await trpc.spr.sklad.getList.query();
-    if (data) this.setState({list: data.list });
-    
-    //const thiDate = await trpc.demo.query(10);
-    console.log(data)
+		this.reloadList();
   }
 
-  onSelectElm = (elmId: number) => {
+  onSelectElm = (elm: ISkladSprElm) => {
     //console.log(elmId+110);
-    this.selectElmId = elmId;
-    this.itemRef.current?.selectItem(elmId);
+    this.selectElmId = elm.id;
+    this.itemRef.current?.selectItem(elm.id);
 
   }
-  onEditElm = (elm: ISklad) => {
-    //console.log(elmId)
-  
+  onEditElm = (elm: ISkladSprElm) => {
+		if(this.props.onChoice) {
+      this.props.onChoice(elm);
+      delNWin(this.props.winId);
+    } else {
       addNWin(SkladElm, {winId: Date.now() ,elmId: this.selectElmId, renew: this.reloadList});
-    
+		}
   }
+
   reloadList = async () => {
     const  data = await trpc.spr.sklad.getList.query();
     console.log(data)
-    if (data) this.setState({ list: data.list });
+    if (data) this.setState({ list: data.list as ISkladSprElm[] });
   }
 
  
-  name() {
-    return 'Товары';
-  }
-  
+	name = () => 'Sklad'
   
   render() {
-    console.log(`render KlientSpr ${this.props.id}`)
+    console.log(`render SkladSpr, Id:${this.props.id}`)
     return (
              
-      <WindowCl winId={this.props.winId} caption='Склады' modal={false} key={this.props.id} >    
-        <div className="container">
-          <div className='buttons'>
+      <WindowCl winId={this.props.winId} caption='Склады' modal={false} key={this.props.winId} size={{width: '200px', height: '300px'}}>    
+        <div className={styles.container}>
+          <div className={styles.button}>
             <SprButtons 
               onNewElm={()=> addNWin(SkladElm, {winId: Date.now(), renew: this.reloadList})}
               />
           </div>
 
-          <div className="table">
-          <ItemTable tableKeys={{head:['Id','Name'], body:['id','name']}} tableData={this.state.list}
+          <div className={styles.table}>
+          <ItemTable<ISkladSprElm>
+						tableKeys={{head:['Id','Name', 'МОЛ'], body:['id','name','mol']}}
+						tableData={this.state.list}
+						skey='id'
           onSelect={this.onSelectElm}
           onEdit={this.onEditElm}
           ref={this.itemRef}
           />
           </div>
         </div>
-        <style jsx>{styles}</style>
+
       </WindowCl>
 
   )
--- trpc/api/spr/sklad/getList.ts
+++ trpc/api/spr/sklad/getList.ts
@@ -9,22 +9,34 @@ import { IClient, IClientCatalog } from '@/db/Entitys/Client'
 export const skladSprGetList = router({
   getList: publicProcedure
     .query(async (opts) => {
-      const { input } = opts;
-
+//      const { input } = opts;
       const { pool } = opts.ctx as Context;
-      let clientList: IClient[] =[{id:0}];
-      let query:string;
 
-        query = `
-          SELECT * FROM sklad
-          ORDER BY name;`;
-          console.log(query)
+      const query = `
+      SELECT 
+      s.id AS sklad_id,
+      s.name AS sklad_name,
+      s.inDoc AS sklad_inDoc,
+      s.mol AS sklad_mol_id,
+      m.id AS mol_id,
+      m.human AS mol_human_id,
+      h.id AS human_id,
+      h.nic AS human_nic
+      FROM sklad AS s
+      LEFT JOIN mol AS m ON m.id = s.mol
+      LEFT JOIN human AS h ON h.id = m.human
+      ORDER BY c.name;`;
+      console.log(query)
       try {
-        const dbClient = await pool.connect();
+        const dbClient:PoolClient = await pool.connect();
         const res = await dbClient.query(query);
         dbClient.release();
-        clientList = res.rows;
-        return {list: clientList, tc: pool.totalCount, ic: pool.idleCount}
+
+        const list = res.rows.map((item) => (
+          { id: item.sklad_id, name: item.sklad_name, inDoc: item.sklad_inDoc, 
+            mol: item.human_nic
+          }));
+        return {list: list, tc: pool.totalCount, ic: pool.idleCount}
       } catch(err) {
         console.log("Error", err);
       }
