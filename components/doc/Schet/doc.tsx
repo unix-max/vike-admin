8cab6fd873e93cfa1ab13f908b1d7e5f55f4e46a
Max Chernyshev <unix_max@mail.ru>
2025-05-11 12:09:01 +0000

    Добавлен документ счет

---
+++ components/doc/Schet/doc.tsx
@@ -0,0 +1,124 @@
+import React from 'react'
+import { trpc } from "@/trpc/client";
+//import {Window, IWinProps} from '../Window'
+import { useWinStore } from '@/pages/+client'
+import { WindowCl } from '../../Window/winCl'
+import { TabPanel, TabItem} from '../../Tabs';
+import { ISchet, ISchetTbl } from '@/db/Entitys/doc/Schet';
+import { SuperInput } from '../../inputs/SuperInput';
+import { SprInput } from '@/components/inputs/SprInput';
+import { FirmSpr } from '@/components/spr/FirmSpr';
+//import styles from './index.module.css'
+const addTWin = useWinStore.getState().addTWin
+const delNWin = useWinStore.getState().delNWin;
+
+type SchetDocProps  = {
+	elmId?: number;
+	renew?: () => void;
+}
+type TWinId = SchetDocProps & { winId: number }
+
+type SchetDocState = {
+	[key: string]: any
+}
+const dt =(ts: number):string => {
+	const date = new Date(ts);
+	const yyyy = date.getFullYear();
+	const mm = (date.getMonth()+1).toString().padStart(2,"0");
+	const dd= date.getDate().toString().padStart(2, "0");
+	return `${dd}/${mm}/${yyyy}`
+}
+//const { data: elm, error: e1 } = useSWR(`/api/spr/client/elm/${props.elmId}`, fetcher);
+export class SchetDoc extends React.Component<TWinId, SchetDocState>{
+	newElmData: SchetDocState;
+	oldHeadData: ISchet;
+	oldTblData: ISchetTbl[];
+
+	constructor(props: TWinId) {
+		super(props);
+		this.newElmData = {};
+		this.oldHeadData = {id:0, date: Date.now()};
+		this.oldTblData = [];
+	}
+
+	async componentDidMount() {
+		if(this.props.elmId) {
+			const data = await trpc.doc.schet.getDoc.query({ id: this.props.elmId});
+			if (data) {
+				this.oldHeadData = data.doc?.head as ISchet;
+				this.oldTblData = data.doc?.tbl as ISchetTbl[];
+			}	
+		this.forceUpdate();
+		}
+	}
+
+changeData= (key: string, val: any) => {
+		if (typeof val == 'object') {
+			this.oldHeadData[key] = val;
+			this.newElmData[key] = val.id;
+			this.forceUpdate();
+		} else {
+			this.oldHeadData[key] = val;
+			this.newElmData[key] = val;
+		}
+		
+	}
+
+	dataSend= async () => {
+		let data: any;
+		try {
+			if (this.props.elmId) {
+				data = await trpc.spr.nds.setElm.mutate({ id: this.props.elmId, ...this.newElmData });
+			} else {
+				data = await trpc.spr.nds.setElm.mutate({ ...this.newElmData });
+			}
+		} catch (e: any) {
+			console.log(e);
+		}
+		if (data && 'elm' in data) {
+			if ( this.props.renew && data?.elm?.id > 0 ) this.props.renew();
+		} else alert(data?.message);
+
+		delNWin(this.props.winId);
+		//console.log(id)	
+
+	}
+	name=() =>'Счет'
+	render() {
+
+		console.log(`Render shetDoc ${this.props.elmId}`)
+		return (           
+		<WindowCl winId={this.props.winId}
+			caption={`Счет №: ${this.oldHeadData.id} от ${this.oldHeadData.date}`}
+			modal={false} key={this.props.winId}>
+			<form className="main">
+        <fieldset>
+          <legend>Основные</legend>
+				
+            <table>
+							<tbody>
+								<tr>
+									<td><SuperInput zagolovok="ID" value={this.oldHeadData.id} onChange={(val) => this.changeData('name', val)}/></td>
+									<td><SuperInput zagolovok="Дата" value={dt(this.oldHeadData.date)} onChange={(val) => this.changeData('val', val)}/></td>
+								</tr>
+								<tr>
+									<td><SprInput zagolovok="Фирма" value={this.oldHeadData.firm?.name} 
+										onBtnClick={() => addTWin(FirmSpr, { onChoice: (elm: any) => this.changeData('firm', elm) })}/></td>
+									<td><SprInput zagolovok="Фирма" value={this.oldHeadData.firm?.name} 
+										onBtnClick={() => addTWin(FirmSpr, { onChoice: (elm: any) => this.changeData('firm', elm) })}/></td>
+								
+												<td><SuperInput zagolovok="Alias" value={this.oldHeadData.descript} onChange={(val) => this.changeData('alias', val)}/></td>
+								</tr>
+							</tbody>
+            </table>
+					
+						
+          </fieldset>
+        </form>
+			<button onClick={this.dataSend}>OK</button>
+			<button onClick={() => delNWin(this.props.winId)}>Отмена</button>
+		</WindowCl>
+	)
+	}
+}
+	
