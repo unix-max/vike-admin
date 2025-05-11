6e84fd804e93b36bb65fc04f1f58a88536be1710
Max Chernyshev <unix_max@mail.ru>
2025-05-11 18:12:09 +0000

    Исправлен firmAcc для соответсвия полю таблицы

--- db/Entitys/doc/Schet.ts
+++ db/Entitys/doc/Schet.ts
@@ -11,7 +11,7 @@ export interface ISchet {
   date: number,
   oldCode?: number,
   firm?: IFirm,
-  firrAcc?: IFirmAccount,
+  firm_acc?: IFirmAccount,
   client?: IClient,
   sklad?: ISklad,
   currency?: ICurrency,
