0e9cae2b5ade7282c87ebe7cc62fe2a7055c53bf
Max Chernyshev <unix_max@mail.ru>
2025-05-11 18:12:50 +0000

    первичная работа шапки док Schet

--- components/doc/Schet/doc.tsx
+++ components/doc/Schet/doc.tsx
@@ -4,10 +4,16 @@ import { trpc } from "@/trpc/client";
 import { useWinStore } from '@/pages/+client'
 import { WindowCl } from '../../Window/winCl'
 import { TabPanel, TabItem} from '../../Tabs';
+import { mainData } from '@/helpers/mainData';
 import { ISchet, ISchetTbl } from '@/db/Entitys/doc/Schet';
 import { SuperInput } from '../../inputs/SuperInput';
 import { SprInput } from '@/components/inputs/SprInput';
 import { FirmSpr } from '@/components/spr/FirmSpr';
+import { FirmAccSpr } from '@/components/spr/FirmAccSpr';
+import { KlientSpr } from '@/components/spr/KlientSpr';
+import { SkladSpr } from '@/components/spr/SkladSpr';
+import { CurrencySpr } from '@/components/spr/CurrencySpr';
+import { NDSSpr } from '@/components/spr/NdsSpr';
 //import styles from './index.module.css'
 const addTWin = useWinStore.getState().addTWin
 const delNWin = useWinStore.getState().delNWin;
@@ -100,14 +106,24 @@ changeData= (key: string, val: any) => {
 								<tr>
 									<td><SuperInput zagolovok="ID" value={this.oldHeadData.id} onChange={(val) => this.changeData('name', val)}/></td>
 									<td><SuperInput zagolovok="Дата" value={dt(this.oldHeadData.date)} onChange={(val) => this.changeData('val', val)}/></td>
+									<td><SuperInput zagolovok="Alias" value={this.oldHeadData.descript} onChange={(val) => this.changeData('alias', val)}/></td>
+						
 								</tr>
 								<tr>
 									<td><SprInput zagolovok="Фирма" value={this.oldHeadData.firm?.name} 
 										onBtnClick={() => addTWin(FirmSpr, { onChoice: (elm: any) => this.changeData('firm', elm) })}/></td>
-									<td><SprInput zagolovok="Фирма" value={this.oldHeadData.firm?.name} 
-										onBtnClick={() => addTWin(FirmSpr, { onChoice: (elm: any) => this.changeData('firm', elm) })}/></td>
-								
-												<td><SuperInput zagolovok="Alias" value={this.oldHeadData.descript} onChange={(val) => this.changeData('alias', val)}/></td>
+									<td><SprInput zagolovok="Счет" value={this.oldHeadData.firm_acc?.name} 
+										onBtnClick={() => addTWin(FirmAccSpr, {firmId: 1, onChoice: (elm: any) => this.changeData('firm_acc', elm) })}/></td>
+									<td><SprInput zagolovok="Клиент" value={this.oldHeadData.client?.name} 
+										onBtnClick={() => addTWin(KlientSpr, { onChoice: (elm: any) => this.changeData('client', elm) })}/></td>
+								</tr>
+								<tr>
+									<td><SprInput zagolovok="Склад" value={this.oldHeadData.sklad?.name} 
+										onBtnClick={() => addTWin(SkladSpr, { onChoice: (elm: any) => this.changeData('sklad', elm) })}/></td>
+									<td><SprInput zagolovok="Валюта" value={this.oldHeadData.currency?.name} 
+										onBtnClick={() => addTWin(CurrencySpr, { onChoice: (elm: any) => this.changeData('currency', elm) })}/></td>
+									<td><SprInput zagolovok="НДС" value={this.oldHeadData.nds?.name} 
+										onBtnClick={() => addTWin(NDSSpr, { onChoice: (elm: any) => this.changeData('nds', elm) })}/></td>
 								</tr>
 							</tbody>
             </table>
